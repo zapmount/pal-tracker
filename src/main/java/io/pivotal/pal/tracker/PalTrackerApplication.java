fbf3af16bea15d943ea2318bad0f378c5794d12b
Ubuntu <ubuntu@ip-172-31-83-76.ec2.internal>
2020-06-10 14:54:39 +0000

    spring MVC ieth REST endpoints

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -2,15 +2,18 @@ package io.pivotal.pal.tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @SpringBootApplication
 public class PalTrackerApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(PalTrackerApplication.class, args);
     }
+
+    @Bean
+    TimeEntryRepository timeEntryRepository() {
+        return new InMemoryTimeEntryRepository();
+    }
 }
-
-
-
-
-
